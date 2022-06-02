f39cb56ca15fe34a98c4f93e04c7b66f6afc1283
RamalataRendani <ramalatarendani.gmail.com>
2022-06-02 14:06:07 +0200

    Add content to file

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cors = require('cors');
+
+//create express app
+var app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+console.log('Books directory is running');
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+
+//create root route
+app.get('/',(req,res) => {
+    res.json({message: 'Welcome to the books directory'})
+});
+
+//setup server port
+const PORT = process.env.PORT || 8081;
+
+//listen to the port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:8081`);
+});
